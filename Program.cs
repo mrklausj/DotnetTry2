440db8f89fd550d4c8442f154b36d48979925427
Klaus Jakobsen <klau2082@elev.tec.dk>
2021-04-20 13:49:06 +0200

    hej med

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetTry2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
